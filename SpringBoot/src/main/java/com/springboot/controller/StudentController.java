5e93eb044e1b1940ded4611d31ed5d8dca31d8e9
katukuri_jaswanth <katukurijaswanth2@gmail.com>
2026-05-06 00:40:08 +0530

    Frontend Vercel deployed

--- SpringBoot/src/main/java/com/springboot/controller/StudentController.java
+++ SpringBoot/src/main/java/com/springboot/controller/StudentController.java
@@ -11,7 +11,13 @@ import java.util.List;
 // This class handles requests from browser/postman
 // and sends back data as JSON automatically
 @RestController
-@CrossOrigin(origins = "http://localhost:5173")
+//@CrossOrigin(origins = "http://localhost:5173")
+//to
+//@CrossOrigin(origins = "*")
+@CrossOrigin(origins = {
+        "http://localhost:5173",
+        "https://student-details-nine-pearl.vercel.app"
+})
 // @RequestMapping means:
 // All URLs in this class start with /api/students
 // Example: http://localhost:8080/api/students
